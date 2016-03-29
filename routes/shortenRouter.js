ad8a9e292aae28ac383f28ff66d9ca20f15bf50b
arialblack14 <tsirasalexis@yahoo.gr>
2016-03-29 11:12:44 +0300

    handle http:// and https:// urls

--- routes/shortenRouter.js
+++ routes/shortenRouter.js
@@ -8,10 +8,22 @@ var shortenRouter = express.Router();
 
 shortenRouter.use(bodyParser.json());
 
+// This takes care of urls like www.example.com
 shortenRouter.route('/:short')
 .get(function(req, res, next) {
   // http://stackoverflow.com/questions/28869001/throw-message-typeerror-first-argument-must-be-a-string-or-buffer
   res.send(shortener.shortener(req, req.params.short));
+});
+
+// Since i get a 404 if user requests for a url like http://www.example.com
+// i implement an additional router function that handles this
+shortenRouter.route('/http\://:short') // escape ":"
+.get(function(req, res, next) {
+  res.send(shortener.httpShortener(req, req.params.short));
+});
+shortenRouter.route('/https\://:short') // escape ":"
+.get(function(req, res, next) {
+  res.send(shortener.httpsShortener(req, req.params.short));
 });
 
 module.exports = shortenRouter;
--- shortener.js
+++ shortener.js
@@ -6,14 +6,36 @@ var isValid = require('./isValid');
 
 // Push all new entries to an array which will hold all shortened urls
 var urlArr = [];
+// Get last url entered
+var last = (urlArr.length -1);
 
 exports.shortener = function(req, url) {
   if (isValid.isValid(url)) {
     urlArr.push(url);
-    // Get last url entered
-    var last = (urlArr.length -1);
+    last++;
     // http://stackoverflow.com/questions/10183291/how-to-get-the-full-url-in-express
     return { "original_url": url , "short_url": (req.protocol + '://' + req.get('host') + "/" + last ) };
+  } else {
+    return "The url you provided is invalid."; // Does not catch all cases but will do for most
+  }
+};
+// Handle request: http:// + url
+exports.httpShortener = function(req, url) {
+  if (isValid.isValid(url)) {
+    urlArr.push(url);
+    last++;
+    return { "original_url": "http://" + url , "short_url": (req.protocol + '://' + req.get('host') + "/" + last ) };
+  } else {
+    return "The url you provided is invalid."; // Does not catch all cases but will do for most
+  }
+};
+
+// Handle request: https:// + url
+exports.httpsShortener = function(req, url) {
+  if (isValid.isValid(url)) {
+    urlArr.push(url);
+    last++;
+    return { "original_url": "https://" + url , "short_url": (req.protocol + '://' + req.get('host') + "/" + last ) };
   } else {
     return "The url you provided is invalid."; // Does not catch all cases but will do for most
   }
